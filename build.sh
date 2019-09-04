dab92daa1594a3066a821296a7615a5a59ebe87f
Harsh Lathwal <xeon.harsh@gmail.com>
2019-09-04 22:09:45 +0530

    changed name and added xorg packages

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -e -u
 
-iso_name=archlinux
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <http://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name=Catergos
+iso_label="Catergos_$(date +%Y%m)"
+iso_publisher="Catergos Linux <http://www.catergos.yogdaan.org>"
+iso_application="Catergos Linux Live/Rescue CD"
 iso_version=$(date +%Y.%m.%d)
 install_dir=arch
 work_dir=work
